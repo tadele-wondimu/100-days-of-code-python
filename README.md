cf6368472fefdec775302db7231f7d397a055507
tade4 <57603171+tade4@users.noreply.github.com>
2025-10-18 18:30:59 +0300

    Day 001: Band name project setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# 100 Days of Python
+My projects from Angela Yu's course.
+
+## Day 001: Variables
+- Band Name Generator: [Run it](day-001-variables/band_name.py)
---
+++ day-001-variables/band_name.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+city_name= input("What city did you grow up in?\n")
+pet_name=input("What is you pet's name?\n")
+print("Your Band name could be: "+ city_name +" "+ pet_name)
+
+
